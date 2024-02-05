f9ea1618056599566c2b754e78393bf8d4b1ddd4
Bishal Singh Deo <bishalsinghdeo5@gmail.com>
2024-02-05 23:14:04 +0530

    update on pixel perfect of header section and first one  rating section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Ratings from '@/components/Ratings'
 import Image from 'next/image'
-import Testimonials from '@/components/Card'
+import Testimonials from '@/components/Testimonials'
 
 
 export default function Home() {
@@ -16,16 +16,16 @@ export default function Home() {
         />
       <div className='z-10'>
         <section className="grid grid-cols-1 lg:grid-cols-2">
-          <article className='justify-center items-center lg:max-w-[28rem] pt-14 lg:pt-14 lg:pl-20 p-4 lg:p-auto'>
-          <h1 className='text-4xl text-primary-very-dark-magenta font-bold text-center lg:text-left lg:pl-8 lg:pt-6'>10,000+ of our users love our products.
+          <article className='justify-center items-center pt-14 lg:pt-12 lg:pl-20 p-4 lg:p-auto lg:max-w-[30rem]'>
+          <h1 className='text-4xl text-primary-very-dark-magenta font-bold text-center lg:text-left lg:pl-[3rem] lg:pt-[2.89rem]'>10,000+ of our users love our products.
           </h1>
-          <p className='font-bold pt-4 text-sm text-neutral-dark-grayish-magenta text-center lg:text-left lg:pl-8'>
+          <p className='font-bold pt-4 text-sm text-neutral-dark-grayish-magenta text-center lg:text-left lg:pl-12'>
           We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services.
           </p>
           </article>
           <Ratings />
         </section>
-        <div className='grid grid-cols-1 lg:grid-cols-3 pb-8 lg:pb-0 pt-11 lg:pl-24 lg:pr-24'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 pb-8 lg:pb-0 pt-11 lg:pt-0 lg:pl-24 lg:pr-24'>
           <Testimonials />
         </div>
       </div>
