53fef6c0039af20277194ba152c8fdea98ffc4c0
Bishal Singh Deo <bishalsinghdeo5@gmail.com>
2024-02-08 19:15:03 +0530

    feat: update pixel perfection

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 
 export default function Home() {
   return (
-    <div className='flex min-h-screen items-center justify-center font-sans'>
+    <div className='flex min-h-screen items-center justify-center p-4'>
       <Image
           src="/assets/bg-pattern-top-desktop.svg"
           alt=""
@@ -14,7 +14,7 @@ export default function Home() {
           className="absolute top-0 w-[100%] h-[32%] md:w-[44%] md:h-[52%] left-0"
           aria-hidden="true"
         />
-      <div className='z-10 font-sans'>
+      <div className='z-10'>
         <section className="grid grid-cols-1 lg:grid-cols-2 lg:max-w-[68rem]">
           <article className='justify-center items-center pt-16 lg:pt-0 lg:pl-0 p-5 lg:p-0 lg:max-w-[25.8rem]'>
           <h1 className='text-4xl lg:text-5xl text-primary-very-dark-magenta font-sans font-bold text-center lg:text-left'>10,000+ of our users love our products.
@@ -25,7 +25,7 @@ export default function Home() {
           </article>
           <Ratings />
         </section>
-        <div className='grid grid-cols-1 lg:grid-cols-3 pt-12 lg:pt-12 lg:pl-0 lg:pr-[0.7rem] lg:max-w-[68rem] pb-8 lg:pb-0'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 pt-12 lg:pt-20 lg:pl-0 lg:pr-[0.7rem] lg:max-w-[68rem] pb-8 lg:pb-0'>
           <Testimonials />
         </div>
       </div>
