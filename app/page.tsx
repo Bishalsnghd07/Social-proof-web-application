f2f1bc9df275fee185ab390d023175dd495d17e6
Bishal Singh Deo <bishalsinghdeo5@gmail.com>
2024-02-08 19:01:44 +0530

    feat: fix pixel perfection

--- app/page.tsx
+++ app/page.tsx
@@ -11,21 +11,21 @@ export default function Home() {
           alt=""
           width={100}
           height={100}
-          className="absolute top-0 w-[100%] h-[32%] md:w-[46%] md:h-[52%] left-0"
+          className="absolute top-0 w-[100%] h-[32%] md:w-[44%] md:h-[52%] left-0"
           aria-hidden="true"
         />
       <div className='z-10 font-sans'>
         <section className="grid grid-cols-1 lg:grid-cols-2 lg:max-w-[68rem]">
-          <article className='justify-center items-center pt-16 lg:pt-2 lg:pl-0 p-5 lg:p-0 lg:max-w-[25.8rem]'>
-          <h1 className='text-4xl lg:text-5xl text-primary-very-dark-magenta font-sans font-bold text-center lg:text-left lg:pl-[3rem]'>10,000+ of our users love our products.
+          <article className='justify-center items-center pt-16 lg:pt-0 lg:pl-0 p-5 lg:p-0 lg:max-w-[25.8rem]'>
+          <h1 className='text-4xl lg:text-5xl text-primary-very-dark-magenta font-sans font-bold text-center lg:text-left'>10,000+ of our users love our products.
           </h1>
-          <p className='font-sans font-semibold pt-4 text-base text-neutral-dark-grayish-magenta text-center lg:text-left lg:pl-12'>
+          <p className='font-sans font-semibold pt-4 text-base text-neutral-dark-grayish-magenta text-center lg:text-left'>
           We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services.
           </p>
           </article>
           <Ratings />
         </section>
-        <div className='grid grid-cols-1 lg:grid-cols-3 pt-12 lg:pt-12 lg:pl-12 lg:pr-[0.7rem] lg:max-w-[68rem] pb-8 lg:pb-0'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 pt-12 lg:pt-12 lg:pl-0 lg:pr-[0.7rem] lg:max-w-[68rem] pb-8 lg:pb-0'>
           <Testimonials />
         </div>
       </div>
--- components/Ratings.tsx
+++ components/Ratings.tsx
@@ -5,16 +5,16 @@ const Ratings = () => {
   return (
     <div>
     <ul>
-      <li className='pl-5 lg:pl-[4.4rem] pr-5 pt-2 lg:pt-[2.2rem]'>
+      <li className='pl-5 lg:pl-[4.4rem] pr-5 pt-2 lg:pt-[1.8rem]'>
     <div className='flex flex-col lg:flex-row bg-neutral-light-grayish-magenta p-4 gap-2 lg:gap-6 justify-center items-center w-full lg:max-w-[24rem] lg:pl-1 lg:pr-6 rounded-lg'>
-    <div className='flex flex-wrap gap-2'>
+      <div className='flex flex-wrap gap-2'>
           <Icon icon="star" 
           aria-hidden="true" />
           <Icon icon="star" aria-hidden="true" />
           <Icon icon="star" aria-hidden="true" />
           <Icon icon="star" aria-hidden="true" />
           <Icon icon="star" aria-hidden="true" />
-    </div>
+      </div>
     <p className='text-center text-primary-very-dark-magenta text-sm font-bold'>Rated 5 Stars in Reviews</p>
     </div>
       </li>
