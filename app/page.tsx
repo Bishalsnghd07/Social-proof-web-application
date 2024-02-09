51bb02f17aca5fd421491108ae20c0e89f516025
Bishal Singh Deo <bishalsinghdeo5@gmail.com>
2024-02-09 19:38:48 +0530

    update main page component

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         />
       <div className='z-10'>
         <section className="grid grid-cols-1 lg:grid-cols-2 lg:max-w-[74rem]">
-          <article className='justify-center items-center pt-16 lg:pt-0 lg:pl-4 p-5 lg:p-0'>
+          <article className='justify-center items-center pt-16 lg:pt-2 lg:pl-6 p-5 lg:p-0'>
           <h1 className='text-4xl lg:text-5xl text-primary-very-dark-magenta font-sans font-bold text-center lg:text-left lg:max-w-[25.8rem]'>10,000+ of our users love our products.
           </h1>
           <p className='font-sans font-medium pt-4 text-lg text-neutral-dark-grayish-magenta text-center lg:text-left lg:max-w-[26.8rem]'>
@@ -25,7 +25,7 @@ export default function Home() {
           </article>
           <Ratings />
         </section>
-        <div className='grid grid-cols-1 lg:grid-cols-3 pt-12 lg:pt-16 lg:pl-4 lg:pr-[0.7rem] lg:max-w-[74rem] pb-8 lg:pb-0'>
+        <div className='grid grid-cols-1 lg:grid-cols-3 pt-12 lg:pt-16 lg:pl-6 lg:pr-[0.7rem] lg:max-w-[74rem] pb-8 lg:pb-0'>
           <Testimonials />
         </div>
       </div>
