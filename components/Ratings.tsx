58083e8b03850a892d3bb3b435d9b7f18d150d50
Bishal Singh Deo <bishalsinghdeo5@gmail.com>
2024-02-10 11:42:26 +0530

    update rartings component

--- components/Ratings.tsx
+++ components/Ratings.tsx
@@ -5,8 +5,8 @@ const Ratings = () => {
   return (
     <div>
     <ul>
-      <li className='pl-5 lg:pl-[1.6rem] pr-5 pt-2 lg:pt-[2.4rem]'>
-    <div className='flex flex-col lg:flex-row bg-neutral-light-grayish-magenta p-[0.82rem] gap-2 lg:gap-6 justify-center items-center w-full lg:max-w-[29.4rem] lg:pr-16 rounded-lg'>
+      <li className='pl-5 lg:pl-[1.6rem] pr-5 pt-2 lg:pt-[2.5rem]'>
+    <div className='flex flex-col lg:flex-row bg-neutral-light-grayish-magenta p-[0.84rem] gap-2 lg:gap-6 justify-center items-center w-full lg:max-w-[29.4rem] lg:pr-16 rounded-lg'>
       <div className='flex flex-wrap gap-2'>
           <Icon icon="star" 
           aria-hidden="true" />
@@ -22,7 +22,7 @@ const Ratings = () => {
 
     <ul>
       <li className='pl-5 lg:pl-[3.8rem] pr-5 pt-5'>
-    <div className='flex flex-col lg:flex-row bg-neutral-light-grayish-magenta p-[0.82rem] gap-2 lg:gap-6 justify-center items-center w-full lg:max-w-[29.4rem] lg:pl-1 lg:pr-6 rounded-lg'>
+    <div className='flex flex-col lg:flex-row bg-neutral-light-grayish-magenta p-[0.84rem] gap-2 lg:gap-6 justify-center items-center w-full lg:max-w-[29.4rem] lg:pl-1 lg:pr-6 rounded-lg'>
     <div className='flex flex-wrap gap-2'>
           <Icon icon="star" 
           aria-hidden="true" />
@@ -37,7 +37,7 @@ const Ratings = () => {
     </ul>
     
      <ul>
-      <li className='pl-5 lg:pl-[5.8rem] pr-5 pt-5'>
+      <li className='pl-5 lg:pl-[5.8rem] pr-5 pt-5 lg:pt-[1.23rem]'>
     <div className='flex flex-col lg:flex-row bg-neutral-light-grayish-magenta p-[0.82rem] gap-2 lg:gap-6 justify-center items-center w-full lg:max-w-[29.4rem] lg:pl-1 lg:pr-6 rounded-lg'>
     <div className='flex flex-wrap gap-2'>
           <Icon icon="star" 
