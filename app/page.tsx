13cb9a92d6ca1c6bdc7e4c9206907f4e92e4e26a
Bishal Singh Deo <bishalsinghdeo5@gmail.com>
2024-02-13 20:13:06 +0530

    added hover effects

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
+"use client";
+
 import Image from 'next/image'
 import Ratings from '@/components/Ratings'
 import Testimonials from '@/components/Testimonials'
+import { motion } from 'framer-motion'
 
 export default function Home() {
+  const textVariants = {
+    initial: {
+      x:-500,
+      opacity: 0,
+    },
+    animate: {
+      x:0,
+      opacity: 1,
+      transition:{
+        duration:1,
+        staggerChildren:0.1,
+      }
+    },
+  }
   return (
     <main className='flex min-h-screen  justify-center items-center bg-neutral-white overflow-hidden'>
       <Image
@@ -15,19 +32,23 @@ export default function Home() {
           loading='eager'
         />
         <div className='z-10'>
-        <section className="grid grid-cols-1 lg:grid-cols-2 lg:max-w-[74rem]">
+        <motion.section className="grid grid-cols-1 lg:grid-cols-2 lg:max-w-[74rem]" variants={textVariants} initial="initial"
+        animate="animate">
           <article className='justify-center items-center pt-16 lg:pt-3 lg:pl-6 p-5 lg:p-0'>
           <h1 className='text-4xl lg:text-5xl text-primary-very-dark-magenta font-sans font-bold text-center lg:text-left lg:max-w-[25.8rem]'>10,000+ of our users love our products.
           </h1>
           <p className=' font-medium pt-4 lg:pt-6 text-lg text-[#a777a5] text-center lg:text-left lg:max-w-[29rem] leading-6'>
-          We only provide great products combined with excellent customer service. See what our satisfied customers are saying about our services.
+          We only provide great <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-pink-500 relative inline-block">
+    <span className="relative text-white">products</span>
+  </span> combined with excellent customer service. See what our satisfied customers are saying about our services.
           </p>
           </article>
           <Ratings />
-          </section>
-          <div className='grid grid-cols-1 lg:grid-cols-3 pt-12 lg:pt-[3.6rem] lg:pl-6 lg:pr-[0.7rem] lg:max-w-[74rem] pb-8 lg:pb-0'>
+          </motion.section>
+          <motion.div className='grid grid-cols-1 lg:grid-cols-3 pt-12 lg:pt-[3.6rem] lg:pl-6 lg:pr-[0.7rem] lg:max-w-[74rem] pb-8 lg:pb-0' variants={textVariants} initial="initial"
+        animate="animate">
           <Testimonials />
-        </div>
+        </motion.div>
         </div>
       <Image
           src="/assets/bg-pattern-bottom-desktop.svg"
--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -4,57 +4,68 @@ const Testimonials = () => {
   return (
     <>
       <ul className='justify-start items-start pl-5 lg:pl-1 lg:pt-[0.22rem] pr-5 lg:pr-4'>
-        <li className=' bg-primary-very-dark-magenta rounded-lg p-[2.36rem]'>
+        <li className='bg-primary-very-dark-magenta rounded-lg p-[2.36rem] shadow-2xl'>
           <figure className='flex flex-wrap gap-5 lg:pt-[0.44rem]'>
           <Image
               src="/assets/image-colton.jpg"
               alt=''
               width={50}
               height={50}
-              className='lg:w-[2.5rem] lg:h-[2.5rem] rounded-full'
+              className='lg:w-[2.5rem] lg:h-[2.5rem] rounded-full border-2 border-orange-400'
             />
             <figcaption className='flex flex-col text-base leading-[1.4rem]'>
-            <strong className='text-white font-semibold'>Colton Smith</strong> 
-            <span className=' text-pink-400'>Verified Buyer</span> 
+            <strong className='text-white font-semibold'>Colton Smith
+            </strong> 
+            <span className='text-pink-400 cursor-pointer group'>Verified Buyer
+            <span className="hidden lg:block bottom-0 h-1 w-0 bg-gradient-to-r from-orange-600 via-white to-green-600 transition-all ease-in-out duration-500 group-hover:w-full"></span> 
+            </span> 
             </figcaption>
-            <blockquote className='text-white font-medium text-[0.96rem] leading-[1.4rem]'>&quot;We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!&quot;</blockquote>
+            <blockquote className='text-white font-medium text-[0.96rem] leading-[1.4rem] select-all selection:text-fuchsia-300'>&quot;We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!&quot;</blockquote>
           </figure>
         </li>
       </ul>
-      <ul className='justify-start items-start pl-5 lg:pl-2 pr-5 lg:pt-[1.29rem] lg:pr-4 pt-4'>
-        <li className='bg-primary-very-dark-magenta rounded-lg p-[2.40rem]'>
+      <ul className='justify-start items-start pl-5 lg:pl-2 pr-5 lg:pt-[1.29rem] lg:pr-4 
+      pt-4'>
+        <li className='bg-primary-very-dark-magenta rounded-lg p-[2.40rem] shadow-2xl'>
           <figure className='flex flex-wrap  gap-5 lg:pt-[0.44rem]'>
           <Image
               src="/assets/image-anne.jpg"
               alt=''
               width={50}
               height={50}
-              className='rounded-full lg:w-[2.5rem] lg:h-[2.5rem]'
+              className='rounded-full lg:w-[2.5rem] lg:h-[2.5rem]  border-2 border-orange-400'
             />
             <figcaption className='flex flex-col text-base leading-[1.4rem]'>
             <strong className='text-white font-semibold'>Anne Wallace</strong> 
-            <span className='text-pink-400'>Verified Buyer</span> 
+            <span className='text-pink-400 group cursor-pointer'>Verified Buyer
+            <span className="hidden lg:block bottom-0 h-1 w-0 bg-gradient-to-r from-orange-600 via-white 
+            to-green-600 transition-all ease-in-out duration-500 group-hover:w-full"></span>
+            </span> 
             </figcaption>
-            <blockquote className='text-white font-medium text-[0.96rem] leading-[1.4rem]'>&quot;Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!&quot;</blockquote>
+            <blockquote className='text-white font-medium text-[0.96rem] leading-[1.4rem] select-all 
+           selection:text-fuchsia-300'>&quot;Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!&quot;</blockquote>
           </figure>
         </li>
       </ul>
 
       <ul className='justify-start items-start pl-5 lg:pl-2 pr-5 lg:pr-4 pt-4 lg:pt-[2.28rem]'>
-        <li className='bg-primary-very-dark-magenta rounded-lg p-[2.38rem]'>
+        <li className='bg-primary-very-dark-magenta rounded-lg p-[2.38rem] shadow-2xl'>
           <figure className='flex flex-wrap  gap-4 lg:pt-[0.44rem]'>
           <Image
               src="/assets/image-irene.jpg"
               alt='Irene Roberts'
               width={50}
               height={50}
-              className='rounded-full lg:w-[2.5rem] lg:h-[2.5rem]'
+              className='rounded-full lg:w-[2.5rem] lg:h-[2.5rem] border-2 border-orange-400'
             />
             <figcaption className='flex flex-col text-base leading-[1.4rem]'>
             <strong className='text-white font-semibold text-base'>Irene Roberts</strong> 
-            <span className='text-pink-400 text-base'>Verified Buyer</span> 
+            <span className='text-pink-400 text-base group cursor-pointer'>Verified Buyer
+            <span className="hidden lg:block bottom-0 h-1 w-0 bg-gradient-to-r from-orange-600 via-white 
+            to-green-600 transition-all ease-in-out duration-500 group-hover:w-full"></span>
+            </span> 
             </figcaption>
-            <blockquote className='text-white font-medium text-[0.96rem] leading-[1.4rem]'>&quot;Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.&quot;</blockquote>
+            <blockquote className='text-white font-medium text-[0.96rem] leading-[1.4rem] select-all selection:text-fuchsia-300'>&quot;Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.&quot;</blockquote>
           </figure>
         </li>
       </ul>
